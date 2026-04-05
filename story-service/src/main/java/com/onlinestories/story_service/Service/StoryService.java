2d80623d12a71756563724c43f32aecb4e7b0240
Khoa1204HCMUT <156278110+mp777888@users.noreply.github.com>
2026-04-05 16:19:28 +0700

    hot fix

--- story-service/src/main/java/com/onlinestories/story_service/Service/StoryService.java
+++ story-service/src/main/java/com/onlinestories/story_service/Service/StoryService.java
@@ -195,8 +195,10 @@ public class StoryService {
                 genres.addAll(newGenres);
             }
             if (img != null && !img.isEmpty()) {
-                String message = mediaClient.deleteFile(story.getImg(), "image");
-                log.info("Deleted old image for story {}: {}", story.getStoryId(), message);
+                if (story.getImg() != null) {
+                    String message = mediaClient.deleteFile(story.getImg(), "image");
+                    log.info("Deleted old image for story {}: {}", story.getStoryId(), message);
+                }
                 String imgUrl = mediaClient.uploadFile(img, "cover-img");
                 story.setImg(imgUrl);
             }
